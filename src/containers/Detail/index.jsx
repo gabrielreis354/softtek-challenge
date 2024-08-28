18be4ce35cc7c3e2cadf85d312de293f389086f0
unknown <gutto-pg@hotmail.com>
2024-08-28 00:49:08 -0300

    details page 90% complete - ready to be recorded to video, missing few details only

--- src/containers/Detail/index.jsx
+++ src/containers/Detail/index.jsx
@@ -174,15 +174,26 @@ export default function Detail() {
             </div>
           </div>
 
-          <div className="details-information">
-            <h3>
-              Atividade
-            </h3>
-            {chamado.Interacoes.map((interacao, index) => (
-              <div className="details-activity"  key={index}>
-                Comentário {index+1}: {interacao}
+          <div className="details-information-col">
+            <div className="details-information">
+              <h3>
+                Atividade
+              </h3>
+              {chamado.Interacoes.map((interacao, index) => (
+                <div className="details-activity"  key={index}>
+                  Comentário {index+1}: {interacao}
+                </div>
+              ))}
+            </div>
+            <div className="details-information">
+              <h3>
+                Resolução
+              </h3>
+              <div className="details-resolution">
+                {chamado.Resolução ? chamado.Resolução : "Não resolvido ainda"}
               </div>
-            ))}
+            </div>
+
           </div>
 
         </div>
@@ -198,7 +209,7 @@ export default function Detail() {
                   <div className="solution" key={index}>
                     <div className="solution-text">
                       <div className="solution-number">
-                        {index+1}
+                        {index+1}.
                       </div>
                       <div className="solution-description">
                         {solucao}
@@ -212,7 +223,6 @@ export default function Detail() {
                   </div>
                 ))
               ) : (
-                <>
 
                 <div className="not-solutions">
                   <div className="not-solutions-text">
@@ -225,11 +235,6 @@ export default function Detail() {
                   </div>
                 </div>
 
-                    
-                    
-
-                </>
-
               )}
             </div>
 
@@ -237,125 +242,6 @@ export default function Detail() {
         </div>
       </div>
 
-      <div className="none">
-        <div className="detail_panel">
-          <div className="tabs">
-            <div className="tab">
-              <h3>
-                <strong>{chamado.Status}</strong>
-              </h3>
-            </div>
-          </div>
-          <h2>Detalhes do Chamado #{chamado.Número}</h2>
-          <div className="items">
-            <div className="item">
-              <div className="item_title">
-                <h4>Abertura</h4>
-              </div>
-              <p>{chamado.Aberto}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Data de Atualização</h4>
-              </div>
-              <p>{chamado.Atualizado}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Encerramento</h4>
-              </div>
-              <p>
-                {chamado.Encerrado ? chamado.Encerrado : "Não resolvido ainda"}
-              </p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Empresa</h4>
-              </div>
-              <p>{chamado.Empresa}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Local</h4>
-              </div>
-              <p>{chamado.Local}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Prioridade</h4>
-              </div>
-              <p>{chamado.Prioridade}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Atribuído</h4>
-              </div>
-              <p>{chamado.Atribuído}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Descrição</h4>
-              </div>
-              <p>{chamado.Descrição}</p>
-            </div>
-            <div className="item">
-              <div className="item_title">
-                <h4>Resolução</h4>
-              </div>
-              <p>
-                {chamado.Resolução ? chamado.Resolução : "Não resolvido ainda"}
-              </p>
-            </div>
-          </div>
-          <h2>Histórico de Interações</h2>
-          <div className="items">
-            {chamado.Interacoes.map((interacao, index) => (
-              <div className="item" key={index}>
-                <div className="item_title">
-                  <h4>Comentário {index + 1}</h4>
-                </div>
-                <p>{interacao}</p>
-              </div>
-            ))}
-          </div>
-
-          <div className="buttons">
-
-            <button className="btn" onClick={handleClose}>
-              Voltar para a lista
-            </button>
-            <button className="btn" onClick={handleSearchSolution}>
-              Procurar Soluções
-            </button>
-          </div>
-        </div>
-
-        <div className="solution">
-          <h2>Soluções</h2>
-
-          <h3>{solutionTitle}</h3>
-          <div className="solucoes_resumo">
-            {solucoesSemelhantes.length > 0 ? (
-              solucoesSemelhantes.map((solucao, index) => (
-                <div className="item" key={index}>
-                  <div className="item_title">
-                    <h3>Solução #{index + 1}</h3>
-                  </div>
-                  <p>{solucao}</p>
-                  <button
-                    className="btn_solution"
-                    onClick={() => handleSelectSolution(solucao)}
-                  >
-                    Aplicar Solução
-                  </button>
-                </div>
-              ))
-            ) : (
-              <p>Nenhuma solução encontrada.</p>
-            )}
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
